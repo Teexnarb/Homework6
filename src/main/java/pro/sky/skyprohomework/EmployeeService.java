13024758effea70e2c87344941a81a7a35c9be8f
User <danilgaybeshev@mail.ru>
2024-02-12 21:16:05 +0500

    Homework6

--- src/main/java/pro/sky/skyprohomework/EmployeeService.java
+++ src/main/java/pro/sky/skyprohomework/EmployeeService.java
@@ -3,40 +3,41 @@ package pro.sky.skyprohomework;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmployeeService {
     private static final int EMPLOYEE_COUNT = 10;
     private int addedEmployees = 0;
-    private final List<Employee> employees = new ArrayList<>(List.of());
+    private final Map<String,Employee> employees = new HashMap<>();
 
     public Employee addEmployee(String firstName, String lastName) {
         if (addedEmployees >= EMPLOYEE_COUNT) {
             throw new EmployeeStorageIsFullException("Сотрудник уже зарегистрирован");
         }
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException("Хранилище заполнено");
         }
-        employees.add(employee);
+        employees.put(employee.getFullName(), employee);
         return employee;
     }
 
     public Employee removeEmployee(String firstName, String lastName) {
         Employee target = new Employee(firstName, lastName);
-        if (employees.remove(target)) {
-            throw new EmployeeNotFoundException("СОтрудник не найден");
+        if (employees.containsKey(target.getFullName())) {
+            return employees.remove(target.getFullName());
         }
-        return target;
+        throw new EmployeeNotFoundException("Сотрудник не найден");
     }
     public Employee containsEmployee(String firstName, String lastName) {
         Employee target = new Employee(firstName, lastName);
-        int targetId = employees.indexOf(target);
-        if (targetId < 0) {
-            throw new EmployeeNotFoundException("Сотрудник не найден");
+        if (employees.containsKey(target.getFullName())) {
+            return employees.get(target.getFullName());
         }
-        return employees.get(targetId);
+        throw new EmployeeNotFoundException("");
     }
 
     public List<Employee> getEmployees() {
